bb4b78e2d9ee3f4c731071a7221c2d2f8c2b9fd7
Gamy Aguas <gamyaguas@berkeley.edu>
2023-08-16 20:12:34 -0700

    finished converting dataset into cvs file

---
+++ Sentiment Analysis/review_ds_processor.py
@@ -0,0 +1,23 @@
+import csv
+import os
+neg_file_dir = './Sentiment Analysis/Movie Review Dataset/tokens/neg'
+pos_file_dir = './Sentiment Analysis/Movie Review Dataset/tokens/pos'  
+neg_txt_files = [file for file in os.listdir(neg_file_dir) if file.endswith('.txt')][:100]
+pos_txt_files = [file for file in os.listdir(pos_file_dir) if file.endswith('.txt')][:100]
+with open('./Sentiment Analysis/movie_review.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+     
+    writer.writerow(["Review", "Polarity"])
+    #writing in the negatives
+    for txt_file in neg_txt_files:
+        with open(os.path.join(neg_file_dir, txt_file), 'r') as file:
+            content = file.read()
+            content = content.replace(",","")
+            writer.writerow([content,0])
+    #writing in the positives
+    for txt_file in pos_txt_files:
+        with open(os.path.join(pos_file_dir, txt_file), 'r') as file:
+            content = file.read()
+            content = content.replace(",","")
+            writer.writerow([content,1])
+     
